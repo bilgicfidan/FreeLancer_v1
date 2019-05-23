0aba95ba528d911c9f8f305be1a24f0484009f5f
bilgicfidan <bilgicfidan@gmail.com>
2019-05-23 06:44:18 +0300

    category controller oluşturuldu
    hatalar giderildi

---
+++ FreeLancer_v1/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using FreeLancer_v1.Models.Managers;
+using FreeLancer_v1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreeLancer_v1.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        DatabaseContext db = new DatabaseContext();
+        // GET: Category
+        public ActionResult Category(int id)
+        {
+            Expert experts = db.Experts.Where(ex => ex.Sub_Category.topCategoryID == id || ex.abilityID==id).FirstOrDefault();
+              
+            return View(experts);
+        }
+    }
+}
