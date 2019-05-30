cd31342d8acd3b4489f87c1dffd61878f3500ae8
Mahmut Yazıcı <yazicimahmut23@gmail.com>
2019-05-30 13:49:23 +0300

    Create Comments.cs

---
+++ FreeLancer_v1.Entities/Comments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeLancer_v1.Entities
+{
+    [Table("Tbl_Yorumlar")]
+    public class Comment : MyEntitiyDatabase
+    {
+        [Required, StringLength(300)]
+        public string Text { get; set; }
+
+        public virtual Notes Notlar { get; set; }
+        public virtual Users Kullanici { get; set; }
+        public Comment()
+        {
+
+        }
+    }
+}
