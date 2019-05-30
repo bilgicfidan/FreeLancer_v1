3f3c4a904704fcdca51e1636149141c0ac79f8f6
Mahmut Yazıcı <yazicimahmut23@gmail.com>
2019-05-30 13:02:39 +0300

    Create Class1.cs
    BusinesLayer oluşturuldu

---
+++ FreeLancer_v1.BusinesLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeLancer_v1.BusinesLayer
+{
+    public class Class1
+    {
+    }
+}
