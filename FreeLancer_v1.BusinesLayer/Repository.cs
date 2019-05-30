052d173a0aa958dfa819a9a01b174cea502289d0
Mahmut Yazıcı <yazicimahmut23@gmail.com>
2019-05-30 14:47:32 +0300

    Create Repository.cs

---
+++ FreeLancer_v1.BusinesLayer/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeLancer_v1.BusinesLayer
+{
+    public class Repository<T> where T : class
+    {
+        private DatabaseContext db = new DatabaseContext();
+        DbSet<T> objectSet;
+
+        public Repository()
+        {
+            objectSet = db.Set<T>();
+        }
+
+        public List<T> List()
+        {
+            return objectSet.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> where)
+        {
+            return objectSet.Where(where).ToList();
+        }
+        public int Insert(T obj)
+        {
+            objectSet.Add(obj);
+            return Save();
+        }
+        public int Update(T obj)
+        {
+            return Save();
+        }
+        public int Delete(T obj)
+        {
+            objectSet.Remove(obj);
+            return Save();
+        }
+        public int Save()
+        {
+            return db.SaveChanges();
+        }
+
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return objectSet.FirstOrDefault(where);
+        }
+    }
+}
+ 
