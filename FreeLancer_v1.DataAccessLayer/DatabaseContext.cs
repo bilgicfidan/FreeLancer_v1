b75420d99ca25e901377c67bed6befd0b2bc468e
Mahmut Yazıcı <yazicimahmut23@gmail.com>
2019-05-30 13:23:11 +0300

    Create DatabaseContext.cs
    DbSetler Eklendi

---
+++ FreeLancer_v1.DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using FreeLancer_v1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeLancer_v1.DataAccessLayer
+{
+   public class DatabaseContext : DbContext
+    {
+        public DbSet<Notes> dB_Notlar { get; set; }
+        public DbSet<Users> dB_Kullanicilar { get; set; }
+        public DbSet<Categories> dB_Kategoriler { get; set; }
+        public DbSet<Comment> dB_Yorumlar { get; set; }
+        public DbSet<Liked> dB_Likes { get; set; }
+
+    }
+}
