a87d0cff6d7d87104bc935f554b128a23613701a
Mahmut Yazıcı <yazicimahmut23@gmail.com>
2019-05-30 15:40:16 +0300

    Update Repository.cs

--- FreeLancer_v1.BusinesLayer/Repository.cs
+++ FreeLancer_v1.BusinesLayer/Repository.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FreeLancer_v1.DataAccessLayer;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
