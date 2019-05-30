04bb959d34e39d27cb7c0461945b0c69fabac7a5
Mahmut Yazıcı <yazicimahmut23@gmail.com>
2019-05-30 14:35:23 +0300

    Create MyEntitiyDatabase.cs

---
+++ FreeLancer_v1.Entities/MyEntitiyDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeLancer_v1.Entities
+{
+    [Table("Tbl_MyEntityDatabase")]
+   public class MyEntitiyDatabase
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required,StringLength(30)]
+        public string ModifiedUserName { get; set; }
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        [Required]
+        public DateTime ModifiedOn { get; set; }
+    }
+}
